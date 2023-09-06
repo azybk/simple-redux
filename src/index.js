cd123f37aba93067d9da8c1984b21c8dfc3f3b91
Azy BK <kumbaraazy@gmail.com>
2023-09-06 21:37:53 +0700

    Redux Toolkit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import store from './store';
+
+// Redux 
+// import store from './store';
+
+// Redux Toolkit
+import store from './redux_toolkit/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
